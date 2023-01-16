886e924fc07e4ed79273381bdebaaad1743c11a8
Yunus Emre Teke <yeteke68@gmail.com>
2023-01-16 23:06:24 +0300

    Base Entity eklendi

---
+++ StockAutomationProject.Entities/Abstract/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAutomationProject.Entities.Abstract
+{
+    internal class BaseEntity
+    {
+    }
+}
---
+++ StockAutomationProject.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StockAutomationProject.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
